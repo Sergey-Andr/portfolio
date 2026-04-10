b5dff34d9628ecb63199cf80854ed72f2b8a6c95
andri <qukviktv@gmail.com>
2026-04-10 20:22:18 +0300

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     webpack: (config) => {
         config.module.rules.push({
             test: /\.pdf$/,
